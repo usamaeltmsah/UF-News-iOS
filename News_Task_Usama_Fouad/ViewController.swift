14e22d12af19e2507ed5b91353ba3b1145af42a2
Usama Fouad <usamaeltmsah@gmail.com>
2022-07-04 11:03:09 +0200

    Initial Commit

---
+++ News_Task_Usama_Fouad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News_Task_Usama_Fouad
+//
+//  Created by Usama Fouad on 04/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
