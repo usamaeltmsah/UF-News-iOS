83504aa3b236728194d5af4957a89f8689bb0c38
Usama Fouad <usamaeltmsah@gmail.com>
2022-11-16 20:38:28 +0200

    Update Constants.swift

--- News_Task_Usama_Fouad/Utilities/Constants.swift
+++ News_Task_Usama_Fouad/Utilities/Constants.swift
@@ -10,7 +10,7 @@ import UIKit
 
 
 struct K {
-    static let apiKey = "2a7bb0f32a634b7883e94b759d7696d9"
+    static let apiKey = {API_KEY}
     
     static var deviceLanguage: String?
     static var localRegionCode: String?
